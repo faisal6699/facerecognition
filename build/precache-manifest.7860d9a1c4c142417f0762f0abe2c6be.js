49943de128233f4078fe2096613d4fd2c733dadd
faisal ahmed <faisal.ahmed6699@gmail.com>
2020-04-19 20:47:17 +0600

    change in gitignore

---
+++ build/precache-manifest.7860d9a1c4c142417f0762f0abe2c6be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d399247febf9cfe367662b861c0a5f",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "9587e4c3d77c32858632",
+    "url": "/facerecognition/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "6b821208a2daecc6674d",
+    "url": "/facerecognition/static/css/main.ec13f725.chunk.css"
+  },
+  {
+    "revision": "9587e4c3d77c32858632",
+    "url": "/facerecognition/static/js/2.66e9fa00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.66e9fa00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b821208a2daecc6674d",
+    "url": "/facerecognition/static/js/main.14824011.chunk.js"
+  },
+  {
+    "revision": "b1c0e8aee0d06b10d4f1",
+    "url": "/facerecognition/static/js/runtime-main.b5d4f63f.js"
+  }
+]);
